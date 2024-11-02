5800f29a61635b7349b2bd8499ef35d849f3dfd5
HP <HP@DESKTOP-29U6424>
2024-11-02 10:17:41 +0530

    vtiger new

--- src/main/java/genericUtilities/JavaUtility.java
+++ src/main/java/genericUtilities/JavaUtility.java
@@ -1,6 +1,37 @@
 package genericUtilities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 public class JavaUtility {
+	/**
+	 * This method will return random number
+	 * @return
+	 */
+	
+	public int randomNumber()
+	{
+		Random r = new Random();
+		int randomNumber = r.nextInt(100);
+		return randomNumber;
+		
+	}
+	
+	/**
+	 * This method will return system date
+	 */
+	
+	public String systemDate()
+	{
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy-hh-mm-ss");
+		String currentDate = formatter.format(date);
+		return currentDate;
+	}
+	
+	
+	
 	
 	
 	
